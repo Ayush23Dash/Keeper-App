ac75c3cb09a1b5ceb0df3bab60ac2bbd10a39f58
Ayush23Dash <ayushshanker23@gmail.com>
2020-02-04 23:56:47 +0530

    Added more functionalities to it like Map,arrow function

---
+++ src/notes.js
@@ -0,0 +1,14 @@
+const notes =[
+    {
+        key : 1,
+        title:"Ayush",
+        content:"How are you"
+    },
+    {
+        key:2,
+        title:"Divyaansh",
+        content:"Must be better"
+    }
+];
+
+export default notes;
